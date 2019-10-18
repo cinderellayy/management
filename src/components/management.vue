265776cb5eb8f219c8cc39234599a63b3b0577ed
yuying <1213513167@qq.com>
2019-10-18 11:04:49 +0800

    左侧菜单切换

--- src/components/management.vue
+++ src/components/management.vue
@@ -22,16 +22,16 @@
               <img src="../assets/folder1.jpg" class="folder-icon" /> 用户中心
             </p>
             <ul v-show="showUser==true">
-              <li :class="{'unit-active':listUnit==1}" @click="listUnit=1">
+              <li :class="{'unit-active':listUnit==1}" @click="listUnit=1;add('用户管理')">
                 <img src="../assets/document_note.png" class="document-icon" /> 用户管理
               </li>
-              <li :class="{'unit-active':listUnit==2}" @click="listUnit=2">
+              <li :class="{'unit-active':listUnit==2}" @click="listUnit=2;add('卖家管理')">
                 <img src="../assets/document_note.png" class="document-icon" /> 卖家管理
               </li>
-              <li :class="{'unit-active':listUnit==3}" @click="listUnit=3">
+              <li :class="{'unit-active':listUnit==3}" @click="listUnit=3;add('商铺管理')">
                 <img src="../assets/document_note.png" class="document-icon" /> 商铺管理
               </li>
-              <li :class="{'unit-active':listUnit==4}" @click="listUnit=4">
+              <li :class="{'unit-active':listUnit==4}" @click="listUnit=4;add('任务审核')">
                 <img src="../assets/document_note.png" class="document-icon" /> 任务审核
               </li>
             </ul>
@@ -41,20 +41,20 @@
               <img src="../assets/folder1.jpg" class="folder-icon" /> 商家辅助
             </p>
             <ul v-show="showShop==true">
-              <li :class="{'unit-active':listUnit==5}" @click="listUnit=5">
+              <li :class="{'unit-active':listUnit==5}" @click="listUnit=5;add('未接监控')">
                 <img src="../assets/document_note.png" class="document-icon" /> 未接监控
               </li>
-              <li :class="{'unit-active':listUnit==6}" @click="listUnit=6">
+              <li :class="{'unit-active':listUnit==6}" @click="listUnit=6;add('未接跟踪')">
                 <img src="../assets/document_note.png" class="document-icon" /> 未接跟踪
               </li>
             </ul>
           </div>
           <div class="list-unit">
-            <p  @click="showList('showData')">
+            <p @click="showList('showData')">
               <img src="../assets/folder1.jpg" class="folder-icon" /> 数据分析
             </p>
             <ul v-show="showData===true">
-              <li :class="{'unit-active':listUnit==7}" @click="listUnit=7">
+              <li :class="{'unit-active':listUnit==7}" @click="listUnit=7;add('单量日报')">
                 <img src="../assets/document_note.png" class="document-icon" /> 单量日报
               </li>
             </ul>
@@ -63,156 +63,158 @@
       </div>
     </div>
     <div class="right-content">
-      <a-tabs @change="callback" type="card" defaultActiveKey="2">
-        <a-tab-pane tab="公告" key="1">公告</a-tab-pane>
-        <a-tab-pane tab="用户管理" key="2">
-          <div class="right-content2">
-            <table class="right-content2-top">
-              <tr>
-                <td>
-                  <span class="top-span">手机号</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">邀请人ID</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">客户编号</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">客户姓名</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">身份证号</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">银行卡号</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">旺旺号</span>
-                  <a-input />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span class="top-span">QQ</span>
-                  <a-input />
-                </td>
-                <td>
-                  <span class="top-span">客户等级</span>
-                  <a-select defaultValue="0" @change="handleChange">
-                    <a-select-option value="0">--全部--</a-select-option>
-                    <a-select-option value="lucy">Lucy</a-select-option>
-                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                    <a-select-option value="Yiminghe">yiminghe</a-select-option>
-                  </a-select>
-                </td>
-                <td>
-                  <span class="top-span">是否审核</span>
-                  <a-select defaultValue="0" @change="handleChange">
-                    <a-select-option value="0">--待审核--</a-select-option>
-                    <a-select-option value="lucy">Lucy</a-select-option>
-                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                    <a-select-option value="Yiminghe">yiminghe</a-select-option>
-                  </a-select>
-                </td>
-                <td>
-                  <span class="top-span">业务员</span>
-                  <a-select defaultValue="0" @change="handleChange">
-                    <a-select-option value="0">--全部--</a-select-option>
-                    <a-select-option value="lucy">Lucy</a-select-option>
-                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                    <a-select-option value="Yiminghe">yiminghe</a-select-option>
-                  </a-select>
-                </td>
-
-                <td>
-                  <span class="top-span">是否冻结</span>
-                  <a-select defaultValue="0" @change="handleChange">
-                    <a-select-option value="0">--全部--</a-select-option>
-                    <a-select-option value="lucy">Lucy</a-select-option>
-                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                    <a-select-option value="Yiminghe">yiminghe</a-select-option>
-                  </a-select>
-                </td>
-                <td>
-                  <span class="top-span">账号审核</span>
-                  <a-select defaultValue="0" @change="handleChange">
-                    <a-select-option value="0">--全部--</a-select-option>
-                    <a-select-option value="lucy">Lucy</a-select-option>
-                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                    <a-select-option value="Yiminghe">yiminghe</a-select-option>
-                  </a-select>
-                </td>
-                <td>
-                  <span class="top-span w100">15天内未接单</span>
-                  <a-checkbox></a-checkbox>
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2">
-                  <span class="top-span">注册时间</span>
-                  <a-range-picker format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" />
-                </td>
-                <td colspan="2">
-                  <span class="top-span">更新时间</span>
-                  <a-range-picker format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" />
-                </td>
-                <td>
-                  <span class>
-                    <a-button type="primary" icon="search">搜索</a-button>
-                    <a-button type="primary" icon="search">导出</a-button>
-                  </span>
-                </td>
-              </tr>
-            </table>
-            <div class="right-content2-detail">
-              <a-table
-                :columns="columns"
-                :dataSource="data"
-                bordered
-                :pagination="{ pageSize: 12 }"
-              >
-                <a slot="审核" slot-scope="text" href="javascript:;" class="table-shenhe">审核</a>
-
-                <template
-                  v-for="col in ['1', '2', '3','4','5','6','7','8','9','10','11','12']"
-                  :slot="col"
-                  slot-scope="text, record, index"
-                >
-                  <div :key="col">
-                    <a-input
-                      v-if="record.editable"
-                      style="margin: -5px 0"
-                      :value="text"
-                      @change="e => handleChange(e.target.value, record.key, col)"
-                    />
-                    <template v-else>{{text}}</template>
-                  </div>
-                </template>
-                <template slot="operation" slot-scope="text, record, index">
-                  <div class="editable-row-operations">
-                    <span v-if="record.editable">
-                      <a @click="() => save(record.key)">Save</a>
-                      <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
-                        <a>Cancel</a>
-                      </a-popconfirm>
-                    </span>
-                    <span v-else>
-                      <a @click="() => edit(record.key)">Edit</a>
-                    </span>
-                  </div>
-                </template>
-              </a-table>
-            </div>
-          </div>
-        </a-tab-pane>
+      <a-tabs
+        hideAdd
+        v-model="activeKey"
+        type="editable-card"
+        @edit="onEdit"
+        @change="callback"
+        defaultActiveKey="1"
+      >
+        <a-tab-pane v-for="pane in panes" :tab="pane.title" :key="pane.key" :closable="pane.closable" :class="{'h600':pane.key!='用户管理'}">{{pane.content}}</a-tab-pane>
       </a-tabs>
+
+      <div class="right-content2" v-show="activeKey=='用户管理'">
+        <table class="right-content2-top">
+          <tr>
+            <td>
+              <span class="top-span">手机号</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">邀请人ID</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">客户编号</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">客户姓名</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">身份证号</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">银行卡号</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">旺旺号</span>
+              <a-input />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="top-span">QQ</span>
+              <a-input />
+            </td>
+            <td>
+              <span class="top-span">客户等级</span>
+              <a-select defaultValue="0" @change="handleChange">
+                <a-select-option value="0">--全部--</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </td>
+            <td>
+              <span class="top-span">是否审核</span>
+              <a-select defaultValue="0" @change="handleChange">
+                <a-select-option value="0">--待审核--</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </td>
+            <td>
+              <span class="top-span">业务员</span>
+              <a-select defaultValue="0" @change="handleChange">
+                <a-select-option value="0">--全部--</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </td>
+
+            <td>
+              <span class="top-span">是否冻结</span>
+              <a-select defaultValue="0" @change="handleChange">
+                <a-select-option value="0">--全部--</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </td>
+            <td>
+              <span class="top-span">账号审核</span>
+              <a-select defaultValue="0" @change="handleChange">
+                <a-select-option value="0">--全部--</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </td>
+            <td>
+              <span class="top-span w100">15天内未接单</span>
+              <a-checkbox></a-checkbox>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <span class="top-span">注册时间</span>
+              <a-range-picker format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" />
+            </td>
+            <td colspan="2">
+              <span class="top-span">更新时间</span>
+              <a-range-picker format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" />
+            </td>
+            <td>
+              <span class>
+                <a-button type="primary" icon="search">搜索</a-button>
+                <a-button type="primary" icon="search">导出</a-button>
+              </span>
+            </td>
+          </tr>
+        </table>
+        <div class="right-content2-detail">
+          <a-table :columns="columns" :dataSource="data" bordered :pagination="{ pageSize: 12 }">
+            <a slot="审核" slot-scope="text" href="javascript:;" class="table-shenhe">审核</a>
+
+            <template
+              v-for="col in ['1', '2', '3','4','5','6','7','8','9','10','11','12']"
+              :slot="col"
+              slot-scope="text, record, index"
+            >
+              <div :key="col">
+                <a-input
+                  v-if="record.editable"
+                  style="margin: -5px 0"
+                  :value="text"
+                  @change="e => handleChange(e.target.value, record.key, col)"
+                />
+                <template v-else>{{text}}</template>
+              </div>
+            </template>
+            <template slot="operation" slot-scope="text, record, index">
+              <div class="editable-row-operations">
+                <span v-if="record.editable">
+                  <a @click="() => save(record.key)">Save</a>
+                  <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
+                    <a>Cancel</a>
+                  </a-popconfirm>
+                </span>
+                <span v-else>
+                  <a @click="() => edit(record.key)">Edit</a>
+                </span>
+              </div>
+            </template>
+          </a-table>
+        </div>
+      </div>
+      
     </div>
     <div class="bottom-content"></div>
   </div>
@@ -231,7 +233,11 @@ export default {
       listUnit1: 5,
       listUnit2: 7,
       data: [],
-      columns: []
+      targetKey: "用户管理",
+      columns: [],
+      activeKey: "用户管理",
+      newTabIndex: 0,
+      panes: []
     };
   },
   mounted() {
@@ -327,33 +333,86 @@ export default {
       });
     }
     this.cacheData = this.data.map(item => ({ ...item }));
+    // 右侧菜单
+    this.panes = [{ title: "用户管理", content: "", key: "用户管理" ,closable:false}];
   },
   methods: {
-    showList(param){
-       if(param == 'showUser'){
-         this.showUser=!this.showUser;
-         if(this.showUser==true){
-         this.listUnit = 1;
-       }
-       }
-       if(param == 'showShop'){
-         this.showShop=!this.showShop;
-         if(this.showShop==true){
-         this.listUnit = 5;
-       }
-       }
-       if(param == 'showData'){
-         this.showData=!this.showData;
-          if(this.showData==true){
-         this.listUnit= 7;
-       }
-       }
-       
-       
-      
+    showList(param) {
+      if (param == "showUser") {
+        this.showUser = !this.showUser;
+        if (this.showUser == true) {
+          // this.listUnit = 1;
+        }
+      }
+      if (param == "showShop") {
+        this.showShop = !this.showShop;
+        if (this.showShop == true) {
+          // this.listUnit = 5;
+        }
+      }
+      if (param == "showData") {
+        this.showData = !this.showData;
+        if (this.showData == true) {
+          // this.listUnit = 7;
+        }
+      }
     },
-    callback(key) {
-      console.log(key);
+    callback(targetKey) {
+      console.log("callback", targetKey);
+      this.targetKey = targetKey;
+    },
+    onEdit(targetKey, action) {
+      this[action](targetKey);
+    },
+    add(title) {
+      const panes = this.panes;
+      var flag = 0;
+      const activeKey = title;
+      panes.forEach(item => {
+        if (title === item.title) {
+          flag++;
+          this.activeKey = activeKey;
+        }
+      });
+      if (flag == 0) {
+        if (title == "用户管理") {
+          panes.push({
+            title: title,
+            content: "",
+            key: activeKey
+          });
+        } else {
+          panes.push({
+            title: title,
+            content: title + "内容",
+            key: activeKey
+          });
+        }
+        this.panes = panes;
+        this.activeKey = activeKey;
+        console.log("add", this.activeKey);
+      }
+    },
+    remove(targetKey) {
+    
+      let activeKey = this.activeKey;
+      let lastIndex;
+      this.panes.forEach((pane, i) => {
+        if (pane.key === targetKey) {
+          lastIndex = i - 1;
+        }
+      });
+      const panes = this.panes.filter(pane => pane.key !== targetKey);
+      if (panes.length && activeKey === targetKey) {
+        if (lastIndex >= 0) {
+          activeKey = panes[lastIndex].key;
+        } else {
+          activeKey = panes[0].key;
+        }
+      }
+      this.panes = panes;
+      this.activeKey = activeKey;
+        console.log("removw", activeKey);
     },
     handleChange(value, key, column) {
       const newData = [...this.data];
@@ -396,8 +455,7 @@ export default {
   float: left;
   width: 180px;
   text-indent: 6px;
-  /* border-right: 1px solid #bdd7f9; */
-  height: 100%;
+  position: absolute;
 }
 .left-title {
   background: #e5f0fe;
@@ -487,5 +545,8 @@ li.unit-active {
   margin-right: 20px;
   background-color: #319eeb !important;
   border-color: #319eeb !important;
+}
+.h600 {
+  min-height: 600px;
 }
 </style>
